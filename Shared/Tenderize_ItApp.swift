ca2cc391a6f794802e1b20ccf8efa6cbb88d132d
lcurtis541 <landoncurtis@u.boisestate.edu>
2022-01-06 17:19:00 -0700

    Initial Commit

---
+++ Shared/Tenderize_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tenderize_ItApp.swift
+//  Shared
+//
+//  Created by Landon on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Tenderize_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
